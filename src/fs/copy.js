73af6026e6a196d1817bb48812766d36e3cfd3b8
Eva Kerrigan <evakerrigan@gmail.com>
2024-01-28 20:23:29 +0600

    fix: change copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import fs from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-  const filePath = "./files";
-  const filePathCopy = "./files_copy";
+  const filePath = path.join(__dirname, "files");
+  const filePathCopy = path.join(__dirname, "files_copy");
 
   try {
     await fs.access(filePath);
