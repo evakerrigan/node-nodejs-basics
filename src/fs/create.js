04793b6d556428d41db7a5ae932683acaeb5bb41
Eva Kerrigan <evakerrigan@gmail.com>
2024-01-28 18:58:00 +0600

    feat: create.js done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = "./files/fresh.txt";
+  const fileContent = "I am fresh and young";
+
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, fileContent);
+    } else {
+      throw err;
+    }
+  }  
 };
 
 await create();
