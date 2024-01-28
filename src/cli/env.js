f84b88ae80edb416c5470764780813abcdd39a1f
Eva Kerrigan <evakerrigan@gmail.com>
2024-01-28 20:57:15 +0600

    feat: env.js done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envPrefix = "RSS_";
+  const envVariables = [];
+
+  for (const key in process.env) {
+    if (key.startsWith(envPrefix)) {
+      envVariables.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  console.log(envVariables.join("; "));
 };
 
 parseEnv();
