935a26cef8d30e5eacde637ba6e7a98b6aa5de21
Eva Kerrigan <evakerrigan@gmail.com>
2024-01-29 04:17:33 +0600

    feat: process develop worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,33 @@
-// n should be received from main thread
+// import { parentPort, workerData, Worker } from 'worker_threads';
+
+// const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+// const sendResult = (data) => {
+//   const result = nthFibonacci(data);
+
+//   if (parentPort) {
+//     parentPort.postMessage(result);
+//   }
+
+//   const worker = new Worker("./worker.js", { workerData: 10 });
+
+//   worker.on("message", (result) => {
+//     console.log("Result:", result); // Результат будет выведен в консоль основного потока
+//   });
+// };
+
+// sendResult(workerData);
+
+import { parentPort, workerData } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (data) => {
+  const result = nthFibonacci(data);
+
+  if (parentPort) {
+    parentPort.postMessage(result);
+  }
 };
 
-sendResult();
+sendResult(workerData);
