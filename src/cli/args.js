490b7da3e58b09248394b9b7abd0519e4fc89b4b
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-28 19:57:23 +0300

    feat: cli task done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code her
+  const cliArgs = process.argv.slice(2);
+
+  const args = [];
+
+  for (let i = 0; i < cliArgs.length; i += 2) {
+    const propName = cliArgs[i].slice(2);
+    const value = cliArgs[i + 1];
+    args.push(`${propName} is ${value}`);
+  }
+
+  console.log(args.join(", "));
 };
 
 parseArgs();
