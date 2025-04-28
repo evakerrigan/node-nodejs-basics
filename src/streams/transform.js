b8eb901da126a16f143703f474f3af2bccb92dc0
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-29 00:11:07 +0300

    feat: streams task done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "stream";
+
+const reverseTransform = new Transform({
+  transform(chunk, _, callback) {
+    callback(null, chunk.toString().split("").reverse().join("") + "\n");
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
