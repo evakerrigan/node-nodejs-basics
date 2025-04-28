73d4c78b52cd4b24b8c4c590407ee20c601dadd1
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-29 01:52:44 +0300

    feat: worker task done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,50 @@
+import os from "os";
+import { fileURLToPath } from "url";
+import path from "path";
+import { Worker } from "worker_threads";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.resolve(__dirname, "worker.js");
+
+const createWorkers = async () => {
+  const coresNumber = os.cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < coresNumber; i++) {
+    const worker = new Worker(filePath, { workerData: 10 + i });
+    workers.push(worker);
+
+    worker.on("message", (result) => {
+      console.log(`Received result from worker ${i}: ${result}`);
+    });
+
+    worker.on("error", (error) => {
+      console.error(`Error in worker ${i}: ${error}`);
+    });
+  }
+
+  const results = await Promise.all(
+    workers.map((worker) => {
+      return new Promise((resolve, reject) => {
+        worker.on("message", (result) => {
+          resolve({ status: "resolved", data: result });
+        });
+
+        worker.on("error", (error) => {
+          resolve({ status: "error", data: null });
+        });
+      });
+    })
+  );
+
+  console.log("Results:", results);
+
+  return results;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const results = await createWorkers();
 };
 
 await performCalculations();
