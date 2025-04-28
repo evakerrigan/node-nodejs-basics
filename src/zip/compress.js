62d238889b14ddbe09e58fcbce23d2bec065bc6e
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-28 21:15:06 +0300

    feat: zip task done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import zlib from "zlib";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files/fileToCompress.txt");
+  const fileOutPath = path.join(__dirname, "files/archive.gz");
+
+  try {
+    await fs.promises.access(fileOutPath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      const readStream = fs.createReadStream(filePath);
+      const writeStream = fs.createWriteStream(fileOutPath);
+      const gzip = zlib.createGzip();
+
+      readStream.pipe(gzip).pipe(writeStream);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await compress();
