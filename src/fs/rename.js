d2097905abed8cb7573c0392d39ba7c0e5fc9a5f
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-28 19:29:32 +0300

    feat: fs task done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  const sourceFilePath = path.join(__dirname, "files/wrongFilename.txt");
+  const newFilePath = path.join(__dirname, "files/properFilename.md");
+
+  try {
+    await fs.access(sourceFilePath);
+    try {
+      await fs.access(newFilePath);
+      console.log("ЭТОТ ФАЙЛ УЖЕ ЕСТЬ");
+      throw new Error("FS operation failed");
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await fs.rename(sourceFilePath, newFilePath);
+        console.log("ФАЙЛ ПЕРЕИМЕНОВАН УСПЕШНО");
+      } else {
+        throw err;
+      }
+    }
+  } catch (err) {
+    console.log("ФАЙЛА ИСТОЧНИКА НЕ СУЩЕСТВУЕТ");
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
