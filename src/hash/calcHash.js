661e69fa892c522f07e97a674c2686a0a39bd0f9
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-29 00:16:11 +0300

    feat: hash task done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import crypto from "crypto";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(filePath);
+  const hash = crypto.createHash("sha256");
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on("end", () => {
+    const hashValue = hash.digest("hex");
+    process.stdout.write(hashValue);
+    console.log(hashValue);
+  });
+
+  readStream.on("error", (error) => {
+    throw new Error("FS operation failed 1");
+  });
 };
 
 await calculateHash();
